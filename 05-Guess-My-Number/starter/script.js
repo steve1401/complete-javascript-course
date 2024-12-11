7f30cbb8f9d6ccb241a6b27a8a46994615085f6f
Steve <86303028+steve1401@users.noreply.github.com>
2024-12-11 20:20:22 +0000

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let highScore;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  } else if (guess == secretNumber) {
+    document.querySelector('.message').textContent = 'Correct 🎉';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too high';
+    score--;
+    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low';
+    score--;
+    document.querySelector('.score').textContent = score;
+  }
+});
